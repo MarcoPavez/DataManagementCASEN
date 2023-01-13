e050aa7f6d89be801811f22217ccece2d449a4f3
MarcoPavez <marcopz03@outlook.com>
2023-01-13 03:05:31 -0300

    tasaEscolaridadStgo

--- script/gestionCASEN2017.R
+++ script/gestionCASEN2017.R
@@ -1,16 +1,28 @@
 library(haven)
 library(tidyverse)
 
-casen2017 <- read_dta("bbdd/casen2017/Casen 2017.dta")
+casen2017 <- read_dta("D:/Proyectos_R/bbdd/casen2017.dta")
+
+exprActualizado <- read_dta("D:/Proyectos_R/bbdd/exprActualizado.dta")
 
 casen2017STGO <- casen2017 %>% filter(region==13)
 
+casen2017STGO <- casen2017STGO %>% inner_join(exprActualizado)
+
+
 casen2017STGO <- casen2017STGO %>% 
-  select(edad,e6a,expr) %>% 
-  group_by(edad,e6a,expr) %>% 
+  select(edad,e6a,expr_C2017_NM) %>% 
+  group_by(edad,e6a,expr_C2017_NM) %>% 
   count()
 
 casen2017STGO <- casen2017STGO %>% 
-  summarise(cantidad=expr*n) %>% 
+  summarise(cantidad=expr_C2017_NM*n) %>% 
   group_by(edad,e6a) %>% 
-  summarise(sum=sum(cantidad))
+  summarise(sum=sum(cantidad)) %>% 
+  filter(edad>=15)
+
+poblacionTotal <- sum(casen2017STGO$sum)
+poblacionEscolarizada <- casen2017STGO %>% filter(e6a!=1 & e6a!=99)
+poblacionEscolarizada <- sum(poblacionEscolarizada$sum)
+
+tasaEscolaridadStgo <- (poblacionEscolarizada/poblacionTotal)*100
